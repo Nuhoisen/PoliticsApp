5243646a03e1229c1eaa9b95f4aa40be25f0835d
nuhoisen <kellyhonsing@gmail.com>
2019-12-08 05:51:59 +0000

    Added main ml model

--- util_scripts/ml_model/main.py
+++ util_scripts/ml_model/main.py
@@ -25,12 +25,19 @@ numpy.random.seed(7)
 
 train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
 
-(train_data, validation_data), test_data = tfds.load(
-    name="imdb_reviews", 
-    split=(train_validation_split, tfds.Split.TEST),
-    as_supervised=True)
 
-print(train_data.batch(10))
+# Tensorflow examples
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
+                          as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+encoder = info.features['text'].encoder
+
+
+
+print("Vocab size: {}".format(encoder.vocab_size))
+
+
 
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
@@ -39,6 +46,7 @@ top_words = 5000
 entry_list = []
 label_list = []
 
+encoded_entry_list = []
 hash_set = set()
 
 with open("abortion_data_and_labels.pkl", 'rb') as pkl_fp:
@@ -55,50 +63,93 @@ with open("abortion_data_and_labels.pkl", 'rb') as pkl_fp:
                     hash_set.add(hash_digest)
                     # If the content is new (no collision)
                     # add it to the list
-                    entry_list.append(entry["content"])                    
+                    entry_list.append(entry["content"])  
+                    encoded_entry_list.append( encoder.encode( entry["content"] ) )
+                    # tf.Tensor(entry["content"], shape =(len(entry["content"]), ) )
                     label_list.append(entry["label"])
 
         except KeyError:
                 continue
+                                    
+# embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
+# hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable=True)
+
+        #######################
+        #######################
+        ### PREPROCESS DATA ###
+        #######################
+        #######################
+            
+            
+            
+        ###########################
+        ###########################
+        ####### TRAIN DATA ########
+        ###########################
+        ###########################
+train_data = encoded_entry_list[:floor(len(encoded_entry_list)/2)]
+train_labels = label_list[:floor(len(label_list)/2)]
+# NUMPY TYPE
+tf_train_data = tf.keras.preprocessing.sequence.pad_sequences(  train_data,
+                                                                padding='post')
+
+#tf_train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=1) 
+tf_train_labels = numpy.array(train_labels)#tf.keras.utils.to_categorical(train_labels, num_classes=1) 
 
 
 
-embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
-hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable=True)
+        ###########################
+        ###########################
+        ######## TEST DATA ########
+        ###########################
+        ###########################
+test_data = encoded_entry_list[ceil(len(encoded_entry_list)/2):]
+test_labels = label_list[ceil(len(label_list)/2):]
+# NUMPY TYPE
+tf_test_data = tf.keras.preprocessing.sequence.pad_sequences(  test_data,
+                                                                padding='post')
 
-print(len(entry_list))
+# tf_test_labels  = tf.keras.utils.to_categorical(test_labels, num_classes=1)                                
+tf_test_labels  = numpy.array(test_labels)#tf.keras.utils.to_categorical(test_labels, num_classes=1)                                
 
-# Build the model
+
+print(tf_train_data.shape)
+print(tf_train_labels.shape)
+
+print(encoder.vocab_size)
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+
+            #######################
+            #######################
+            ### Build the model ###
+            #######################
+            #######################
 model = tf.keras.Sequential()
-model.add(hub_layer)
-model.add(tf.keras.layers.Dense(16, activation='relu'))
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+model.add(tf.keras.layers.Embedding(input_dim=encoder.vocab_size, output_dim=32, input_length=tf_train_data.shape[1], mask_zero=True)) # Embedding layer
+model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)))
+model.add(tf.keras.layers.Dense(32, activation='relu'   ))
+model.add(tf.keras.layers.Dense(1,  activation='sigmoid'))
 
 
 print(model.summary())
 
 
-# Extract a training set and a testing set
-train_data = entry_list[:floor(len(entry_list)/2)]
-train_labels = label_list[:floor(len(label_list)/2)]
 
-test_data = entry_list[ceil(len(entry_list)/2):]
-test_labels = label_list[ceil(len(label_list)/2):]
-
-
-
-model.compile(optimizer='adam',
+model.compile(  optimizer='adam',
                 loss='binary_crossentropy',
                 metrics=['accuracy'])
                 
 
-model.fit(  x=train_data, 
-            y=train_labels, 
-            epochs=10, 
-            batch_size=32,
-            verbose=1 )
+model.fit(      x = tf_train_data,
+                y = tf_train_labels,
+                epochs = 10,
+                verbose = 1 )
+
+
             
-results = model.evaluate( x=test_data, y=test_labels, batch_size=32, verbose=2)
+results = model.evaluate( x = test_data, y = test_labels, batch_size=32, verbose=2)
 for name, value in zip(model.metrics_names, results):
     print("%s: %.3f" %(name, value))
 
@@ -107,24 +158,3 @@ for name, value in zip(model.metrics_names, results):
 # print(len(entry_list))
 # (x_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
 
-
-# print(x_train[0])
-# print(y_train[0])
-	
-# # truncate and pad input sequences
-# max_review_length = 500
-# x_train = sequence.pad_sequences(x_train, maxlen=max_review_length)
-# X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
-
-# embedding_vecor_length = 32
-# model = Sequential()
-# model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-# model.add(LSTM(100))
-# model.add(Dense(1, activation='sigmoid'))
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# print(model.summary())
-# model.fit(x_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
-
-# # Final evaluation of the model
-# scores = model.evaluate(X_test, y_test, verbose=0)
-# print("Accuracy: %.2f%%" % (scores[1]*100))
