b2eae9222f0f4b61243e92e1715927d3275bb79c
nuhoisen <kellyhonsing@gmail.com>
2020-01-06 07:09:31 +0000

    Added bill retrieval tasks to flask server

--- BackEnd/flask_server.py
+++ BackEnd/flask_server.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
 from flask_cors import CORS
 from sql_retriever import SqlRetriever
+from sql_storer import SqlStorer
+import datetime
+
+
 from newspaper import Article
 import json
 
@@ -15,13 +19,15 @@ politician_sql_table_name = "politiciantable"
 
 news_sql_db_name = "newsarticlesdb"
 news_sql_table_name = "politician_based_newsarticlestable"
-# Dummy Function
+
+
+
     
-@app.route('/request_state/<state>', methods=['GET'])    
-def request_state(state):
-    return state
-    
-    
+# ------------------------------------------------
+# ------------------------------------------------
+# ---------------- POLITICIAN DATA ---------------
+# ------------------------------------------------
+# ------------------------------------------------
 @app.route('/request_senator_profile_img/', methods=['GET'])    
 def request_senator_profile_img():
     state = request.args.get('state')
@@ -63,6 +69,7 @@ def convert_response_2_dict(res):
         prof_dict["Facebook"] = row.FacebookURL
         prof_dict["Email"] = row.EmailAddress
         prof_dict["Bio"] = row.Bio
+        prof_dict["VoteSmartID"] =  row.VoteSmartID
         prof_list.append(prof_dict)
     return prof_list
         
@@ -81,6 +88,8 @@ def request_state_politician_profile():
     
     
     return json.dumps(prof_list)
+
+
 
 @app.route('/request_wildcard_match/', methods=['GET'])
 def request_wildcard_match():
@@ -110,7 +119,119 @@ def request_state_partisanship():
     print(partisan_dict)
     # prof_list = convert_response_2_dict(response)
     return json.dumps(partisan_dict)
+
+
+
+
+# ------------------------------------------------
+# ------------------------------------------------
+# ---------------- BILL DATA ---------------------       
+# ------------------------------------------------
+# ------------------------------------------------
+cand_bills_db_name = "PoliticianInfo"
+cand_bills_table_name = "politician_voting_record_table"
+
+bill_info_table_name = "bill_info_table"
+
+
+# SQL Retriever
+poly_voting_record_sql_retriever = SqlStorer(db_name=cand_bills_db_name, table_name=cand_bills_table_name)
+poly_voting_record_sql_retriever.set_up_connection()
+
+bill_info_sql_retriever = SqlStorer(db_name=cand_bills_db_name, table_name=bill_info_table_name)
+bill_info_sql_retriever.set_up_connection() 
+ 
+ 
+def convert_bills_response_2_detailed_dict(cand_bill_record_res):
     
+    # Ultimate list containing dictionaries of detailed bill information
+    bills_list = []
+    
+    # Dictionary used for collecting all total fields
+    bill_dict = {}
+    
+    wild_card_query = {}
+    
+    ############################################################
+    ##### Iterate through all of the Bills the candidate has_key
+    ##### ever voted on ########################################
+    ############################################################
+    for cand_bill_record_row in cand_bill_record_res:
+        ################################################
+        ######## Enter immediately known fields ########
+        ################################################
+        bill_dict['PoliticianVote']             = cand_bill_record_row.Vote
+        bill_dict['PoliticianSponsored']        = cand_bill_record_row.Sponsored
+        bill_dict['VoteSmartBillID']            = cand_bill_record_row.VoteSmartBillID
+        ################################################
+        ########## Set Up The Wild Card Query ##########
+        ################################################
+        wild_card_query['VoteSmartBillId']      = cand_bill_record_row.VoteSmartBillID
+        bill_res = bill_info_sql_retriever.retrieve_by_wildcard_and(wild_card_query)
+        
+        ################################################
+        ######## This should only iterate once #########
+        ################################################
+        for bill_row in bill_res:
+            bill_dict['BillTitle']                      = bill_row.BillTitle
+            bill_dict['BillNumber']                     = bill_row.BillNumber
+            bill_dict['BillHighlights']                 = bill_row.BillHighlights
+            bill_dict['BillSynopsis']                   = bill_row.BillSynopsis
+            bill_dict['BillType']                       = bill_row.BillType
+            bill_dict['State']                          = bill_row.State
+            
+            bill_dict['VoteSmartPrimaryCategoryId']     = bill_row.VoteSmartPrimaryCategoryId
+            bill_dict['VoteSmartPrimaryCategoryName']   = bill_row.VoteSmartPrimaryCategoryName
+            bill_dict['DateIntroduced']                 = bill_row.DateIntroduced
+            bill_dict['IntroducedIn']                   = bill_row.IntroducedIn
+            
+            bill_dict['SenateYea']                      = bill_row.SenateYea
+            bill_dict['SenateNay']                      = bill_row.SenateNay
+            bill_dict['SenateDemocratYea']              = bill_row.SenateDemocratYea
+            bill_dict['SenateDemocratNay']              = bill_row.SenateDemocratNay
+            bill_dict['SenateRepublicanYea']            = bill_row.SenateRepublicanYea
+            bill_dict['SenateRepublicanNay']            = bill_row.SenateRepublicanNay
+            
+            bill_dict['HouseYea']                       = bill_row.HouseYea
+            bill_dict['HouseNay']                       = bill_row.HouseNay
+            bill_dict['HouseDemocratYea']               = bill_row.HouseDemocratYea
+            bill_dict['HouseDemocratNay']               = bill_row.HouseDemocratNay
+            bill_dict['HouseRepublicanYea']             = bill_row.HouseRepublicanYea
+            bill_dict['HouseRepublicanNay']             = bill_row.HouseRepublicanNay
+            break
+        
+        bills_list.append(bill_dict.copy())
+        bill_dict.clear()
+            
+        
+    return bills_list       
+ 
+# ------------------------------------
+# ---- Used for date time conversion
+# ------------------------------------
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__() 
+
+@app.route('/request_candidates_bills/', methods=['GET'])
+def request_candidates_bills():
+    wild_card_query = {}
+    cand_id = request.args.get("VoteSmartCandID")
+    wild_card_query['VoteSmartCandID'] = cand_id
+    
+    response = poly_voting_record_sql_retriever.retrieve_by_wildcard_and(wild_card_query)
+    
+    bills_list = convert_bills_response_2_detailed_dict(response)
+    
+    return json.dumps(bills_list, default = myconverter)
+    
+    
+
+# ------------------------------------------------
+# ------------------------------------------------
+# ---------------- NEWS DATA ---------------------     
+# ------------------------------------------------
+# ------------------------------------------------   
 def convert_news_company(name):
     name= name.strip()
     new_name = ""
@@ -123,10 +244,13 @@ def convert_news_company(name):
         "oregon_live" : "Oregon Live",
         "washington_post" : "Washington Post",
         "politico" : "Politico",
+        
         "miami_herald" : "Miami Herald",
         "new_jersey_com" : "New Jersey AP",
         "npr" : "NPR",
-        "new_york_times": "New York Times"
+        "new_york_times": "New York Times",
+        "new_yorker" : "The New Yorker",
+        "the_sacramento_bee" : "The Sacramento Bee"
     }
 
     try:
@@ -137,8 +261,7 @@ def convert_news_company(name):
     return new_name
     
     
-    
-    
+   
 
 def convert_rows_2_list(rows):
     art_list = []
@@ -169,11 +292,14 @@ def request_articles():
     
     return json.dumps(art_list)
 
-    
-    
-# ---------------- MAP DATA ---------------------
+# ------------------------------------------------
+# ------------------------------------------------
+# ---------------- MAP DATA ----------------------
+# ------------------------------------------------
+# ------------------------------------------------
 @app.route('/request_map/<map_name>', methods=['GET'])
 def request_map(map_name):
+    print("Request_made")
     f = open(map_name, 'r')
     f_data = f.read()
     return f_data
@@ -206,6 +332,11 @@ def request_state_senate(state_id):
     f_data = f.read()
     return f_data
 
+# ------------------------------------------------
+# ------------------------------------------------
+# ------------------------------------------------
+# ------------------------------------------------
+# ------------------------------------------------
 
-    
-#app.run(host='0.0.0.0')    
+# Remove when run with gunicorn    
+app.run(host='0.0.0.0')    
