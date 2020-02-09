a771e4f965106b3e00328f243cba1978f624b830
nuhoisen <kellyhonsing@gmail.com>
2020-02-09 05:26:32 +0000

    Added updated ml main and pkl record

--- util_scripts/ml_model/main.py
+++ util_scripts/ml_model/main.py
@@ -34,7 +34,6 @@ model = None
 # CHECKPOINT PATH
 checkpoint_path = os.getcwd() + "\checkpoint.ckpt"
 
-# <<<<<<< HEAD
 
 # Tensorflow examples
 dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
@@ -43,22 +42,6 @@ train_dataset, test_dataset = dataset['train'], dataset['test']
 
 encoder = info.features['text'].encoder
 
-
-<<<<<<< HEAD
-# Tensorflow examples
-dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
-                          as_supervised=True)
-train_dataset, test_dataset = dataset['train'], dataset['test']
-
-encoder = info.features['text'].encoder
-
-
-
-print("Vocab size: {}".format(encoder.vocab_size))
-
-
-=======
->>>>>>> 2cfad074eb9d2e376ece4d19bec8863c23207a28
 
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
@@ -68,10 +51,7 @@ entry_list = []
 label_list = []
 
 encoded_entry_list = []
-<<<<<<< HEAD
-=======
 
->>>>>>> 2cfad074eb9d2e376ece4d19bec8863c23207a28
 hash_set = set()
 
 with open("abortion_data_and_labels.pkl", 'rb') as pkl_fp:
@@ -90,18 +70,13 @@ with open("abortion_data_and_labels.pkl", 'rb') as pkl_fp:
                     # add it to the list
                     entry_list.append(entry["content"])  
                     encoded_entry_list.append( encoder.encode( entry["content"] ) )
-                    # tf.Tensor(entry["content"], shape =(len(entry["content"]), ) )
-<<<<<<< HEAD
-=======
+                   
 
->>>>>>> 2cfad074eb9d2e376ece4d19bec8863c23207a28
                     label_list.append(entry["label"])
 
         except KeyError:
                 continue
-                                    
-# embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
-# hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable=True)
+
 
         #######################
         #######################
@@ -121,18 +96,9 @@ train_labels = label_list[:floor(len(label_list)/2)]
 # NUMPY TYPE
 tf_train_data = tf.keras.preprocessing.sequence.pad_sequences(  train_data,
                                                                 padding='post')
-<<<<<<< HEAD
 
-#tf_train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=1) 
-tf_train_labels = numpy.array(train_labels)#tf.keras.utils.to_categorical(train_labels, num_classes=1) 
-=======
->>>>>>> 2cfad074eb9d2e376ece4d19bec8863c23207a28
+tf_train_labels = numpy.array(train_labels)
 
-#tf_train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=1) 
-tf_train_labels = numpy.array(train_labels)#tf.keras.utils.to_categorical(train_labels, num_classes=1) 
-
-
-<<<<<<< HEAD
         ###########################
         ###########################
         ######## TEST DATA ########
@@ -144,39 +110,16 @@ test_labels = label_list[ceil(len(label_list)/2):]
 tf_test_data = tf.keras.preprocessing.sequence.pad_sequences(  test_data,
                                                                 padding='post')
 
-# tf_test_labels  = tf.keras.utils.to_categorical(test_labels, num_classes=1)                                
 tf_test_labels  = numpy.array(test_labels)#tf.keras.utils.to_categorical(test_labels, num_classes=1)                                
 
-
-print(tf_train_data.shape)
-print(tf_train_labels.shape)
-
-print(encoder.vocab_size)
 BUFFER_SIZE = 10000
 BATCH_SIZE = 64
-=======
 
-        ###########################
-        ###########################
-        ######## TEST DATA ########
-        ###########################
-        ###########################
-test_data = encoded_entry_list[ceil(len(encoded_entry_list)/2):]
-test_labels = label_list[ceil(len(label_list)/2):]
-# NUMPY TYPE
-tf_test_data = tf.keras.preprocessing.sequence.pad_sequences(  test_data,
-                                                                padding='post')
->>>>>>> 2cfad074eb9d2e376ece4d19bec8863c23207a28
-
-# tf_test_labels  = tf.keras.utils.to_categorical(test_labels, num_classes=1)                                
-tf_test_labels  = numpy.array(test_labels)#tf.keras.utils.to_categorical(test_labels, num_classes=1)                                
-
-<<<<<<< HEAD
-            #######################
-            #######################
-            ### Build the model ###
-            #######################
-            #######################
+        #######################
+        #######################
+        ### Build the model ###
+        #######################
+        #######################
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Embedding(input_dim=encoder.vocab_size, output_dim=32, input_length=tf_train_data.shape[1], mask_zero=True)) # Embedding layer
 model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)))
@@ -185,45 +128,20 @@ model.add(tf.keras.layers.Dense(1,  activation='sigmoid'))
 
 
 print(model.summary())
-=======
+# =======
 
-# print(tf_train_data.shape)
-# print(tf_train_labels.shape)
->>>>>>> 2cfad074eb9d2e376ece4d19bec8863c23207a28
 
 # print(encoder.vocab_size)
 BUFFER_SIZE = 10000
 BATCH_SIZE = 64
 
 
-<<<<<<< HEAD
-model.compile(  optimizer='adam',
-                loss='binary_crossentropy',
-                metrics=['accuracy'])
-                
-
-model.fit(      x = tf_train_data,
-                y = tf_train_labels,
-                epochs = 10,
-                verbose = 1 )
-
-
-            
-results = model.evaluate( x = test_data, y = test_labels, batch_size=32, verbose=2)
-for name, value in zip(model.metrics_names, results):
-    print("%s: %.3f" %(name, value))
-
-
-# print(len(label_list))
-# print(len(entry_list))
-# (x_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
-=======
-
-    #######################
-    #######################
-    ### Build the model ###
-    #######################
-    #######################
+    
+        #######################
+        #######################
+        ### Build the model ###
+        #######################
+        #######################
             
 def create_model(train_x_set, train_y_set):
     global model
@@ -285,6 +203,7 @@ for arg in sys.argv:
     if arg == "1":
         create_model(train_x_set=tf_train_data, train_y_set=tf_train_labels)
         fit_new_model(train_x_set=tf_train_data, train_y_set=tf_train_labels)
+        
     elif arg == "2":
         create_model(train_x_set=tf_train_data, train_y_set=tf_train_labels)
         run_saved_model(train_x_set=tf_train_data, train_y_set=tf_train_labels)
@@ -301,5 +220,4 @@ for arg in sys.argv:
         print("\t\t 1 : create_model")
         print("\t\t 2 : load_saved_model")
         print("\t\t 3 : load_saved_model_no_weights")
->>>>>>> 2cfad074eb9d2e376ece4d19bec8863c23207a28
 
